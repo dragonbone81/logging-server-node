15a89abe703283feefab10b79201808a6f4b6579
Christian <cvernikoff@gmail.com>
2019-01-03 09:49:22 -0800

    input

--- index.js
+++ index.js
@@ -3,5 +3,5 @@ const app = express();
 const server = app.listen(3000, () => console.log("Server Started!"));
 
 app.get('/', (req, res) => {
-    res.json({ sucess: true, message: 0 });
+    res.json({ sucess: true, message: 0, input: false });
 })
