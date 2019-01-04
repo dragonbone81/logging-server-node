6e1c8132ed78f1c75a8452c894cb2c423c90468d
Christian <cvernikoff@gmail.com>
2019-01-04 14:26:52 -0800

    added confirm dialog

--- client/src/ViewPosts.js
+++ client/src/ViewPosts.js
@@ -8,6 +8,8 @@ class ViewPosts extends Component {
         log_clicked: false,
         deleting_log: false,
         getting_logs: false,
+        confirm_dialog: false,
+        log_clicked_id: null,
     }
     componentDidMount() {
         this.load_logs();
@@ -17,7 +19,15 @@ class ViewPosts extends Component {
         const logs = await API.get_logs();
         this.setState({ logs, getting_logs: false })
     }
-    delete_log = async (id) => {
+    open_confirm = (id) => {
+        this.setState({ confirm_dialog: true, log_clicked_id: id });
+    }
+    close_confirm = () => {
+        this.setState({ confirm_dialog: false, log_clicked_id: null });
+    }
+    delete_log = async () => {
+        const id = this.state.log_clicked_id;
+        this.close_confirm();
         this.setState({ log_clicked: id, deleting_log: true })
         const response = await API.delete_log(id);
         if (response) {
@@ -28,6 +38,16 @@ class ViewPosts extends Component {
     render() {
         return (
             <div className="ViewPosts">
+                {this.state.confirm_dialog ? <div className='confirm-top'>
+                    <div className='react-confirm-alert'>
+                        <div className='confirm-body'>
+                            <div className="confirm-text">Are you sure?</div>
+                            <br />
+                            <button onClick={this.delete_log} className="pure-button pure-button-primary">Yes</button>
+                            <button onClick={this.close_confirm} className="pure-button pure-button-primary button-confirm-no">No</button>
+                        </div>
+                    </div>
+                </div> : null}
                 <h2>Your Posts <i onClick={this.load_logs} className="fas fa-sync refresh-logs hover-cursor"></i></h2>
                 <table className="pure-table pure-table-horizontal log-table">
                     <thead>
@@ -50,7 +70,7 @@ class ViewPosts extends Component {
                                             this.state.log_clicked === log._id && this.state.deleting_log ?
                                                 <i className="fas fa-cog fa-spin delete-log-spinner" />
                                                 :
-                                                <div onClick={() => this.delete_log(log._id)} className="delete-log">x</div>
+                                                <div onClick={() => this.open_confirm(log._id)} className="delete-log">x</div>
                                         }
                                     </td>
                                 </tr>
