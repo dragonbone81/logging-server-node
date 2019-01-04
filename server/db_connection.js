14b517764747d140388acc7483d1e7acc80fc706
Christian <cvernikoff@gmail.com>
2019-01-04 07:03:23 -0800

    added build

--- server/db_connection.js
+++ server/db_connection.js
@@ -2,7 +2,7 @@ const MongoClient = require('mongodb').MongoClient;
 const assert = require('assert');
 
 // Connection URL
-const url = process.env.DB_URL || "url";
+const url = process.env.DB_URL || "mongodb+srv://admin:iwonjoey@dragonbone81-logging-server-zm6je.mongodb.net/test?retryWrites=true";
 
 // Use connect method to connect to the Server
 const client = MongoClient.connect(url, { useNewUrlParser: true })
