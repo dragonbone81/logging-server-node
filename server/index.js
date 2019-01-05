1dece6157bb3570a80518d0aac1001bb52361665
Christian <cvernikoff@gmail.com>
2019-01-05 09:53:03 -0800

    test

--- server/index.js
+++ server/index.js
@@ -17,7 +17,7 @@ app.get("/", async (req, res) => {
   // console.log(client.db("logging-app"));
   // console.log(app.locals.db);
   // app.locals.db.insertOne({"time":"sdfsdf", "type":"asdasd"});
-  // const x = queries.add_log(req.app.locals.db, { "asd": "asd" });
+  // /const x = queries.add_log(req.app.locals.db, { "asd": "asd" });
   // const x = new Timestamp();
   res.json({ sucess: true, message: 0, input: true, SECRET_KEY });
 });
